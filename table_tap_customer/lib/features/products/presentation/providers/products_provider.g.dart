ed14753bffd9aa3531af7beec2b91e4afe47c1bc
Edwin Vargas <vargas11742@gmail.com>
2024-02-24 16:11:52 -0500

    listar productos de fb e ir al detalle

---
+++ table_tap_customer/lib/features/products/presentation/providers/products_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'products_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productsListHash() => r'26c944667d5619e817647fd25887e748e2e036fb';
+
+/// See also [ProductsList].
+@ProviderFor(ProductsList)
+final productsListProvider =
+    NotifierProvider<ProductsList, List<Product>>.internal(
+  ProductsList.new,
+  name: r'productsListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$productsListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ProductsList = Notifier<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- table_tap_customer/lib/features/products/presentation/screens/products_screen.dart
+++ table_tap_customer/lib/features/products/presentation/screens/products_screen.dart
@@ -1,40 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:table_tap_customer/config/config.dart';
+import 'package:table_tap_customer/features/products/domain/domain.dart';
+import 'package:table_tap_customer/features/products/presentation/providers/providers.dart';
 
-class ProductsScreen extends StatefulWidget {
+class ProductsScreen extends ConsumerStatefulWidget {
   const ProductsScreen({super.key});
 
   @override
-  State<ProductsScreen> createState() => _ProductsScreenState();
+  ProductsScreenState createState() => ProductsScreenState();
 }
 
-class _ProductsScreenState extends State<ProductsScreen> {
-  String text = "asd";
+class ProductsScreenState extends ConsumerState<ProductsScreen> {
   List productsList = [];
   @override
+  void initState() {
+    super.initState();
+    // ref.read(productsListProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    FirebaseFirestore db = FirebaseFirestore.instance;
-    Future<void> get() async {
-      CollectionReference collectionReferenceProducts =
-          db.collection("products");
+    final loadNextPage = ref.read(productsListProvider.notifier).loadNextPage;
+    final setProduct = ref.read(productSelectedProvider.notifier).setProduct;
 
-      QuerySnapshot queryProducts = await collectionReferenceProducts.get();
-      queryProducts.docs.forEach((element) {
-        productsList.add(element.data());
-        print(element.id);
-        print(element.data());
-      });
-      text = productsList[0]["name"];
-      print(text);
-      setState(() {});
-    }
-
-    return Scaffold(
-      body: Container(
-        child: Center(child: Text(text)),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () => get()),
-      // onPressed: () => context.push("/product"), child: Text("GO")),
+    List<Product> productsList = ref.watch(productsListProvider);
+    ThemeColors palette = ThemeColors.palette();
+    final snackBar = SnackBar(
+      duration: const Duration(seconds: 2),
+      backgroundColor: palette.main,
+      content: const Text('Producto agregado'),
     );
+    return Scaffold(
+        appBar: AppBar(title: Text("Lsita de productos")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: loadNextPage,
+          child: Icon(Icons.refresh),
+        ),
+        body: ListView.builder(
+          itemCount: productsList.length,
+          itemBuilder: (context, index) {
+            Product product = productsList[index];
+
+            return ListTile(
+              title: Text(product.dish.name),
+              leading: IconButton(
+                  onPressed: () {
+                    context.push(RoutesNames.product);
+                    setProduct(product);
+                  },
+                  icon: Icon(Icons.remove_red_eye)),
+              trailing: IconButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).clearSnackBars();
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  icon: Icon(Icons.add)),
+            );
+          },
+        )
+        // floatingActionButton: FloatingActionButton(onPressed: () => get()),
+        // onPressed: () => context.push("/product"), child: Text("GO")),
+        );
   }
 }
