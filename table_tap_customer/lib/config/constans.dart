ee3f865ca31e596885684f41f2b1706e1a33173a
Edwin Vargas <vargas11742@gmail.com>
2024-02-24 15:36:24 -0500

    constantes

---
+++ table_tap_customer/lib/config/constans.dart
@@ -0,0 +1,10 @@
+class DbNames {
+  static const products = "products";
+  static const users = "users";
+  static const orders = "ordes";
+}
+
+class RoutesNames {
+  static const products = "/";
+  static const product = "/product";
+}
