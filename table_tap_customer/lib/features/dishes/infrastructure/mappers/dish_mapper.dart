e5838792988f463acd2f18e7c73faf5afe7681c2
Edwin Vargas <vargas11742@gmail.com>
2024-03-01 00:21:28 -0500

    fix: dishes

--- table_tap_customer/lib/features/dishes/infrastructure/mappers/dish_mapper.dart
+++ table_tap_customer/lib/features/dishes/infrastructure/mappers/dish_mapper.dart
@@ -9,4 +9,15 @@ class DishMapper {
         photos: List<String>.from(json["photos"].map((x) => x)),
         ingredients: List<String>.from(json["ingredients"].map((x) => x)),
       );
+
+        static Map<String, dynamic> toJson(Dish dish) => {
+        "idDish": dish.idDish,
+        "name": dish.name,
+        "price": dish.price,
+        "amount": dish.amount,
+        "photos": List<dynamic>.from(dish.photos.map((x) => x)),
+        "ingredients": dish.ingredients != null
+            ? List<dynamic>.from(dish.ingredients!.map((x) => x))
+            : [],
+      };
 }
