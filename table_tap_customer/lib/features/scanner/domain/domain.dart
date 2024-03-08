a67ef129d422bd53a670d563af98272a307d985b
jorgeprograma <jemartelo07@gmail.com>
2024-03-07 21:13:59 -0500

    Cambio en el login mensaje de error

--- table_tap_customer/lib/features/scanner/domain/domain.dart
+++ table_tap_customer/lib/features/scanner/domain/domain.dart
@@ -1,5 +1,4 @@
 export 'entities/table_entity.dart';
 export 'datasources/tables_datasource.dart';
-export './repositories/Tables_repository.dart';
 export 'datasources/table_local_storage_datasource.dart';
 export 'repositories/table_local_storage_repository.dart';
